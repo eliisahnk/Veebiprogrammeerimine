aa13da4fd75e1c0125a7bab09aa08fca22b65259
Eliisa Hanko <eliihan@users.noreply.github.com>
2018-09-10 10:40:52 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Eliisa Hanko, õppetöö</title>
+</head>
+<body>
+  <h1>Eliisa Hanko</h1>
+  <p>Siin on <a href="http://www.tlu.ee" target-"_blank">TLÜ</a>  
+  õppetöö raames valminud veebilehed. 
+  Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+  <p>Üks uus asi minu kohta.</p>
+  <!--<img src=
+  "http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+  <img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+  <p>Mul on sõber, kes ka teeb  <a href="../~merivor">veebi</a>.</p>
+  
+  
+  
+  
+
+</body>
+</html>
